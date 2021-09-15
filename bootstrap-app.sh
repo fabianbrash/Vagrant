b1ef20cb937a81fbe7b27f5d48bee32b68e7b8fc
Fabian Brash <fabianbrash@gmail.com>
2021-09-15 15:02:07 -0400

    Create bootstrap-app.sh

---
+++ bootstrap-app.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get upgrade -y
+apt-get install -y python3 python3-pip
+touch /etc/pip.conf
+
+cat <<EOF > /etc/pip.conf
+[global]
+trusted-host = pypi.python.org
+               pypi.org
+               files.pythonhosted.org
+
+EOF
+
+pip3 install --upgrade pip
