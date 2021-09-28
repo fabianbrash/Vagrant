4b9d356dc7535f86811928b37e0593d7a906e100
Fabian Brash <fabianbrash@gmail.com>
2021-09-28 19:20:07 -0400

    fixed typo

--- bootstrap-node-env-source.sh
+++ bootstrap-node-env-source.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y curl python3 g++ make zip
 curl -kLO https://nodejs.org/dist/v14.18.0/node-v14.18.0.tar.gz
 tar -xzvf node-v14.18.0.tar.gz
-cd node-14.18.0/
+cd node-v14.18.0/
 ./configure
 make -j4
 make install
